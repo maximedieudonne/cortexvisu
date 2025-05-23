f664b47d5d3308a4e433dee783248cb7075d7024
maximedieudonne <maximedieudonne@protonmail.com>
2025-05-23 15:13:34 +0200

    add fonction parser

--- cortexvisu/parser.py
+++ cortexvisu/parser.py
@@ -1,3 +1,13 @@
 import nibabel as nib
 import numpy as np
 
+def load_gii_surface(filepath):
+    gii = nib.load(filepath)
+    coords = gii.darrays[0].data.tolist()
+    faces = gii.darrays[1].data.tolist()
+    return coords, faces
+
+def load_scalar_texture(filepath):
+    return np.loadtxt(filepath).tolist()
+
+
