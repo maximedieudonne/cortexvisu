ee30ad0b6c7f27dfe1a17c19edf9696c1a5623c8
Yashovardhan Thevalil <yashthevalil@gmail.com>
2025-11-27 19:33:00 +0100

    Stuff

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -1,4 +1,5 @@
 def test_surface_loading():
     from cortexvisu.parser import load_gii_surface
+
     coords, faces = load_gii_surface('tests/test_surface.gii')
     assert len(coords) > 0 and len(faces) > 0
